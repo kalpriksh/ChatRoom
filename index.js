042e7f353f20c2026e26f457868becc216c6da25
Kalpriksh Bist <kalpriksh.bist@cygrp.com>
2019-08-10 09:51:42 +0530

    added files

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function(){
+  console.log('listening on port 3000');
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('A user has connected '+ socket.id);
+});
