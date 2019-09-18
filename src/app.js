3a312de0ee32bebad56ec231631823813e82f77a
Kalpriksh Bist <kalpriksh.bist@cygrp.com>
2019-09-18 15:16:00 +0530

    environment variables port

--- src/app.js
+++ src/app.js
@@ -91,7 +91,9 @@ io.on('connection',(socket)=>{
 });
 
 
-server.listen(3000, ()=>{
-    console.log('server running on port 3000');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+server.listen(port);
 
